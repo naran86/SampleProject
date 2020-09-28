14cb9691d480cea1a06000f35af7a883869778b7
naran86 <72000111+naran86@users.noreply.github.com>
2020-09-28 14:54:06 +0530

    Add files via upload

---
+++ R/hellonew.R
@@ -0,0 +1,21 @@
+#' @title Hello, worldview!
+#' @description Simple Package
+#' @examples hellonew()
+#' @export hellonew
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hellonew <- function() {
+  print("Hello, new world!")
+}
